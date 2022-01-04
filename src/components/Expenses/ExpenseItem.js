f790433483361668e16071dafff440d11c77d4e7
Ionut <i.poterasu@yahoo.com>
2022-01-04 15:24:41 +0200

    second commit

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,23 @@
+import React, { useState } from 'react';
+
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
+    // first element = value itself, second element = the updated function
+    const [title, setTitle] = useState(props.title);    // useState ALWAYS return an array with 2 elements
+
     const clickHandler = () => {
-        console.log('Clicked!!')
-    }
+        setTitle('Updated');
+        console.log(title);
+    };
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
             <div className='expense-item__description'>
-                <h2>{props.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>${props.amount}</div>
             </div>        
             <button onClick={clickHandler}>Change Title</button>    
